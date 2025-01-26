de78ae6245ec51c520bc597f9aa79c080cd72b27
Aknur Abdimalik <aknurabdimalik@gmail.com>
2025-01-26 22:31:57 +0500

    LAB1

--- lab1/1.py
+++ lab1/1.py
@@ -1,1 +1,21 @@
-print("helloo")
+print("Hello, World!")
+
+#2
+import sys
+
+print(sys.version)
+
+#3
+if 7 > 6:
+  print("Seven is greater than six!")
+if 7 > 6:
+    print("Seven is greater than six!")
+
+#4
+x = 5
+y = "Hello, World!"
+
+print(x)
+print(y)
+
+    
